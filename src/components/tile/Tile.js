e642dadebc570a4c6e62e04a891bd845de6702ba
tjupp <travisjupp@gmail.com>
2023-06-18 22:22:49 -0600

    TileList renders a Tile component for each property-value in the data-source

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({name, description}) => {
   return (
     <div className="tile-container">
+      <p className="tile-title">{name}</p>
+      {
+        description.map(element => {
+          return <p className="tile">{element}</p>
+        })
+      }
       
     </div>
   );
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({contacts}) => {
   return (
     <div>
-      
+      {contacts.map(({name, ...description},i) => {
+        return (
+          <Tile name={name} description={Object.values(description)} key={i+name.split(' ')[0]+name.split(' ')[1]} />
+        );
+      })}
     </div>
   );
 };
