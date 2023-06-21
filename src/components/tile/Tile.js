b66dd2e4e4998a65d9f59d5cf3fc58d2c32b5bf7
tjupp <travisjupp@gmail.com>
2023-06-21 07:20:28 -0600

    add key attribute to p elements in Tile component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -3,10 +3,10 @@ import React from "react";
 export const Tile = ({name, description}) => {
   return (
     <div className="tile-container">
-      <p className="tile-title">{name}</p>
+      <p className="tile-title tile">{name}</p>
       {
-        description.map(element => {
-          return <p className="tile">{element}</p>
+        description.map((element,i) => {
+          return <p key={i} className="tile">{element}</p>
         })
       }
       
