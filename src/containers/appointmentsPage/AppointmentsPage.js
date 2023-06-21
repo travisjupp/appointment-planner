5aee9e6e2da413f144d73955256b2a042acee796
tjupp <travisjupp@gmail.com>
2023-06-21 07:32:53 -0600

    stateful AppointmentsPage component renders AppointmentForm

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,28 +3,40 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = ({ appointments, addAppointment, contacts }) => {
   /*
   Define state variables for 
   appointment info
   */
+  const [title, setTitle] = useState('');
+  const [contact, setContact] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
-  };
+    let formData = new FormData(e.target);
+    let appointment = Object.fromEntries(formData.entries());
+    console.log('appointment', appointment);
+    addAppointment(appointment);
 
+
+  };
+  console.log('contact', contact);
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm title={title} setTitle={setTitle} contacts={contacts} contact={contact} setContact={setContact}
+          date={date} setDate={setDate} time={time} setTime={setTime} handleSubmit={handleSubmit} />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList contacts={appointments} />
       </section>
     </div>
   );
