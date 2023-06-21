4bdccb609093d340b143cbfa2fb5fe4f82b5dc46
tjupp <travisjupp@gmail.com>
2023-06-21 07:18:52 -0600

    contact select-menu moved to ContactPicker component

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({contact, setContact, contacts}) => {
+
   return (
-    <></>
+    <>
+      <label>Contact: {contact}
+        <select
+          value={contact}
+          onChange={e => setContact(e.target.value)}
+          name="contact"
+        >
+          <option key="default" value="">No Contact Selected</option>
+          {
+            contacts.map((el, i) => {
+              return <option key={i} value={el.name}>{el.name}</option>
+            })
+          }
+        </select>
+      </label>
+    </>
   );
 };
